a8e8fe7f09f9e9627a64d42723b611b9420e2859
nitin singh <nsinghboss02@gmail.com>
2024-06-23 17:16:38 +0530

    backend added

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="footer">
-      <h4 className="text-center">All Right Reserved © Ecommerce - Jackets</h4>
+      <h4 className="text-center">All Right Reserved © Easy - Jackets</h4>
       <p className="text-center mt-3">
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
